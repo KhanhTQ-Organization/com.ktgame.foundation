75ced34881e98945cf5e62d5522ab9948b91f7be
KhanhTQ-Uni <khanhquang0912qh@gmail.com>
2025-03-11 14:32:51 +0700

    fix: unity version 2020

--- Runtime/Collections/Pool.cs
+++ Runtime/Collections/Pool.cs
@@ -6,18 +6,18 @@ namespace com.ktgame.foundation.collections
 {
     public class Pool<T> where T : new()
     {
-        private static Pool<T> _shared = new();
+        private static Pool<T> _shared = new Pool<T>();
 
         public static Pool<T> Shared => _shared;
 
-        private readonly Queue<T> _queue = new();
+        private readonly Queue<T> _queue = new Queue<T>();
 
 #if UNITY_EDITOR
         /// <seealso href="https://docs.unity3d.com/Manual/DomainReloading.html"/>
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
         private static void InitializeOnLoad()
         {
-            _shared = new();
+            _shared = new Pool<T>();
         }
 #endif
 
